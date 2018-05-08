d4d485139c9f773753c84d4f547b33aa2945d4ea
sammouse <sammouse36@gmail.com>
2018-05-08 19:24:43 +0530

    final commit

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-console.log('starting app');
+console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
-console.log(user);
-fs.appendFileSync('greetings.txt', 'hello ' + user.username + ' your dir is ' + user.homedir );          
+var addresult=notes.addfun(2,3);
+console.log(addresult);
+
+
+// var res=notes.addnote();
+// console.log(res);
+
+// var user = os.userInfo();
+// console.log(user);  
+
+//fs.appendFileSync('greetings.txt', 'hello ' + user.username + ' your dir is ' + user.homedir );           
+//another way to append
+// fs.appendFileSync('greetings.txt',`hello ${user.username} your age is  ${notes.age}`);  
---
+++ notes.js
@@ -0,0 +1,11 @@
+console.log('starting notes.js');
+
+module.exports.addfun=function(a,b){
+    return a+b;
+}
+
+
+// module.exports.addnote= () =>{
+//     console.log('addnote');
+//     return'new note';
+// }
